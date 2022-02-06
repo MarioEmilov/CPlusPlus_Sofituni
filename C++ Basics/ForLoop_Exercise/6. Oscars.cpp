0023604ead53baef34a63d38e6bf1360986f5b7f
Mario Emilov <88974458+MarioEmilov@users.noreply.github.com>
2022-02-06 15:04:31 +0200

    Add files via upload

---
+++ C++ Basics/ForLoop_Exercise/6. Oscars.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string actorName;
+    double startingPoints;
+    int evaluatorsNumber;
+
+    getline(cin >> ws, actorName);
+    cin >> startingPoints >> evaluatorsNumber;
+
+    string evaluatorName;
+    double evaluatorPoints;
+
+    cout.setf(ios::fixed);
+    cout.precision(1);
+
+    for (int i = 0; i < evaluatorsNumber; i++)
+    {
+        getline(cin >> ws, evaluatorName);
+        cin >> evaluatorPoints;
+
+        int evaluatorNameLength = evaluatorName.length();
+
+        startingPoints += (evaluatorPoints * evaluatorNameLength) / 2.0;
+
+        if (startingPoints >= 1250.5)
+        {
+            cout << "Congratulations, " << actorName << " got a nominee for leading role with " << startingPoints << "!" << endl;
+
+            return 0;
+        }
+    }
+
+    double diff = 1250.5 - startingPoints;
+
+    cout << "Sorry, " << actorName << " you need " << diff << " more!" << endl;
+}
